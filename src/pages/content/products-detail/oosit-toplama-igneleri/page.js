5fb4ac7e2aad0f8158d3191e9bc19dbaa6d83bba
enverenes <enver.enes98@gmail.com>
2024-12-01 21:11:14 +0300

    photo upload 2

--- src/pages/content/products-detail/oosit-toplama-igneleri/page.js
+++ src/pages/content/products-detail/oosit-toplama-igneleri/page.js
@@ -3,10 +3,10 @@ import { ArrowRight } from 'lucide-react';
 import ProductGrid from '../gridcomponent';
 
 const products = [
-  { webpath: '/sense', name: 'SENSE', brand: 'Vitrolife', path: '' },
-{ webpath: '/single-lumen', name: 'SINGLE LUMEN™', brand: 'Vitrolife', path: '' },
-{ webpath: '/double-lumen', name: 'DOUBLE LUMEN™', brand: 'Vitrolife', path: '' },
-{ webpath: '/igne-aksesuarlari', name: 'İĞNE AKSESUARLARI', brand: 'Vitrolife', path: '' }
+  { webpath: '/sense', name: 'SENSE', brand: 'Vitrolife', path: 'SENSE-2.png' },
+{ webpath: '/single-lumen', name: 'SINGLE LUMEN™', brand: 'Vitrolife', path: 'single-lumen.png' },
+{ webpath: '/double-lumen', name: 'DOUBLE LUMEN™', brand: 'Vitrolife', path: 'double-lumen.png' },
+{ webpath: '/igne-aksesuarlari', name: 'İĞNE AKSESUARLARI', brand: 'Vitrolife', path: 'aksesuar.png' }
 ];
 
 export default function Home() {
